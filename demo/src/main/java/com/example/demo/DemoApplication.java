5748767ff9050ff7abb3d306c759f0c1283c8aaa
zzeo14 <wodh7223@cau.ac.kr>
2024-05-01 21:08:31 +0900

    initial commit

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class TestController {
+        @GetMapping("/test")
+        public String test(){
+            return "Hello, World!";
+        }
+}
