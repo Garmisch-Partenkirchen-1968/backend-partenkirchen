e30272bcb6bc5003c468ef7c4c4708d4b4c81df1
Youngho Cho <dominoyh5@gmail.com>
2024-05-11 17:45:27 +0900

    add: Comment entity

---
+++ src/main/java/com/example/demo/entity/Comment.java
@@ -0,0 +1,27 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne
+    private User commenter;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
