576668ba1127d82ad33e9d508073fe615a2f8c74
Youngho Cho <dominoyh5@gmail.com>
2024-05-12 15:13:16 +0900

    move: controller package

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.example.demo.entity.controller;
+package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
