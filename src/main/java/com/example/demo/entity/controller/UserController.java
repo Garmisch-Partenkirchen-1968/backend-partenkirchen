ca3e05024b17890d689f923999ff6f56f9b2b32c
Youngho Cho <dominoyh5@gmail.com>
2024-05-12 13:43:37 +0900

    add: /signin

--- src/main/java/com/example/demo/entity/controller/UserController.java
+++ src/main/java/com/example/demo/entity/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.example.demo.entity.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
     @PostMapping("/signup")
-    public String signup() {
-        return "success";
+    public void signup() {
+    }
+
+    @GetMapping("/signin")
+    public void signin() {
     }
 }
