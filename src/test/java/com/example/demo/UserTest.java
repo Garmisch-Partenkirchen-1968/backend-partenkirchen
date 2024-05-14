66417094b0152af76b8875b5717631133e08628d
Youngho Cho <dominoyh5@gmail.com>
2024-05-14 23:44:15 +0900

    add: test for signin and singup

--- src/test/java/com/example/demo/UserTest.java
+++ src/test/java/com/example/demo/UserTest.java
@@ -1,16 +1,20 @@
 package com.example.demo;
 
 import com.example.demo.controller.UserController;
+import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,6 +24,9 @@ public class UserTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private UserService userService;
 
@@ -27,12 +34,52 @@ public class UserTest {
     private UserRepository userRepository;
 
     @Test
-    void wrongParameterWhenSignUp() throws Exception {
-        this.mockMvc.perform(post("/signup")).andDo(print()).andExpect(status().is4xxClientError());
+    void signUp() throws Exception {
+        User user = new User().builder().username("test-admin").password("test-admin").build();
+        this.mockMvc.perform(post("/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andDo(print())
+                .andExpect(status().isCreated());
     }
 
     @Test
-    void wrongParameterWhenSignIp() throws Exception {
-        this.mockMvc.perform(post("/signin")).andDo(print()).andExpect(status().is4xxClientError());
+    void signUpWithWhitespace() throws Exception {
+        User user = new User().builder().username("test admin").password("test-admin").build();
+        this.mockMvc.perform(post("/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void signUpWithEmptyUsername() throws Exception {
+        User user = new User().builder().username("").password("test-admin").build();
+        this.mockMvc.perform(post("/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void signUpWithEmptyPassword() throws Exception {
+        User user = new User().builder().username("").password("test-admin").build();
+        this.mockMvc.perform(post("/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void signInWithUnexistUser() throws Exception {
+        User user = new User().builder().username("wrongusernameasdflkjhasdflkjha").password("aadmin").build();
+        this.mockMvc.perform(get("/signin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
     }
 }
