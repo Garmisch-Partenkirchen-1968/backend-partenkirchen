5b07158f4b65b3c59071a2217db92fc8b1c661eb
zzeo14 <wodh7223@cau.ac.kr>
2024-05-15 00:22:11 +0900

    update: rename, reqname -> username

--- src/main/java/com/example/demo/dto/project/PermissionRequest.java
+++ src/main/java/com/example/demo/dto/project/PermissionRequest.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @Getter
 public class PermissionRequest {
-    private String reqname;
+    private String username;
     private String password;
     private boolean[] permissions;
 }
