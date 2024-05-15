568ba88c8de06c1dd090526c777f063b8a45ea83
Youngho Cho <dominoyh5@gmail.com>
2024-05-15 11:17:57 +0900

    remove: unused import

--- src/test/java/com/example/demo/UserTest.java
+++ src/test/java/com/example/demo/UserTest.java
@@ -1,17 +1,12 @@
 package com.example.demo;
 
-import com.example.demo.controller.UserController;
 import com.example.demo.entity.User;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
