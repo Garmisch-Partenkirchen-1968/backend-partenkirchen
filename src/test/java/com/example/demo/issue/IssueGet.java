f97a2495a3d6ae4e8e5968285a14b63462c8b334
Youngho Cho <dominoyh5@gmail.com>
2024-05-15 16:02:34 +0900

    rename: IssuesGet to IssueGet

--- src/test/java/com/example/demo/issue/IssueGet.java
+++ src/test/java/com/example/demo/issue/IssueGet.java
@@ -31,7 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles
 @AutoConfigureMockMvc
 @Transactional
-public class IssuesGet {
+public class IssueGet {
     @Autowired
     private MockMvc mockMvc;
 
