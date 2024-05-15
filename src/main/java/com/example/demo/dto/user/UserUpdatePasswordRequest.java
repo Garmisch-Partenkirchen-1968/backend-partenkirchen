66ca59b1477ae3858c411660757f71e6e55ff05a
Youngho Cho <dominoyh5@gmail.com>
2024-05-15 12:15:58 +0900

    add: new test case for patch user

--- src/main/java/com/example/demo/dto/user/UserUpdatePasswordRequest.java
+++ src/main/java/com/example/demo/dto/user/UserUpdatePasswordRequest.java
@@ -1,10 +1,12 @@
 package com.example.demo.dto.user;
 
 import com.example.demo.entity.User;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
+@Builder
 public class UserUpdatePasswordRequest {
     private String username;
     private String password;
--- src/test/java/com/example/demo/UserTest.java
+++ src/test/java/com/example/demo/UserTest.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.dto.user.UserUpdatePasswordRequest;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,8 +14,11 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -118,9 +122,97 @@ public class UserTest {
     }
 
     @Test
+    @DisplayName("없는 계정의 비밀번호를 바꾸려 한 경우")
+    void updatePasswordWithNotExistUser() throws Exception {
+        // 내 계정 생성
+        User userSignUp = User.builder().username("test").password("test").build();
+        userService.signUpUser(userSignUp);
+
+        User user = User.builder().username("test").password("test").build();
+        this.mockMvc.perform(patch("/user/98273")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)))
+            .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("다른 사람의 비밀번호를 바꾸려 한 경우")
+    void updatePasswordAnotherUser() throws Exception {
+        // 내 계정 생성
+        User userSignUp = User.builder().username("test").password("test").build();
+        userService.signUpUser(userSignUp);
+
+        // 다른 사람 계정 생성
+        User anotherUserSignUp = User.builder().username("another").password("another").build();
+        anotherUserSignUp = userService.signUpUser(anotherUserSignUp);
+
+        User user = User.builder().username("test").password("test").build();
+        this.mockMvc.perform(patch("/user/" + anotherUserSignUp.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("비밀번호를 바꾸려는데 기존 비밀번호가 틀린 경우")
+    void updatePasswordWithWrongPassword() throws Exception {
+        // 내 계정 생성
+        User userSignUp = User.builder().username("test").password("test").build();
+        userSignUp = userService.signUpUser(userSignUp);
+
+        User user = User.builder().username("test").password("wrongpassword").build();
+        this.mockMvc.perform(patch("/user/" + userSignUp.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("새로운 비밀번호가 empty string인 경우")
+    void updatePasswordWithEmptyPassword() throws Exception {
+        // 내 계정 생성
+        User userSignUp = User.builder().username("test").password("test").build();
+        userSignUp = userService.signUpUser(userSignUp);
+
+        // 비밀번호 변경 시도
+        UserUpdatePasswordRequest user = UserUpdatePasswordRequest.builder()
+                .username("test")
+                .password("test")
+                .newPassword("")
+                .build();
+        this.mockMvc.perform(patch("/user/" + userSignUp.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("성공적으로 비밀번호를 바꾸는 경우")
+    void updatePassword() throws Exception {
+        // 내 계정 생성
+        User userSignUp = User.builder().username("test").password("test").build();
+        userSignUp = userService.signUpUser(userSignUp);
+
+        // 비밀번호 변경 시도
+        UserUpdatePasswordRequest user = UserUpdatePasswordRequest.builder()
+                .username("test")
+                .password("test")
+                .newPassword("newpassword")
+                .build();
+        this.mockMvc.perform(patch("/user/" + userSignUp.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
     @DisplayName("없는 계정을 삭제하려고 하는 경우")
     void deleteNotExistUser() throws Exception {
-        User user = User.builder().username("test-admin").password("test-admin").build();
+        // 내 계정 생성
+        User userSignUp = User.builder().username("test").password("test").build();
+        userService.signUpUser(userSignUp);
+
+        User user = User.builder().username("test").password("test").build();
         this.mockMvc.perform(delete("/user/3484539")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(user)))
