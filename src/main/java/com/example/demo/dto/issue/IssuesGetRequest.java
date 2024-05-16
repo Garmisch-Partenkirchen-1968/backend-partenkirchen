7fc2982216ce56c6947d1883335053c2914951ac
zzeo14 <wodh7223@cau.ac.kr>
2024-05-16 17:43:11 +0900

    update: delete null assign

--- src/main/java/com/example/demo/dto/issue/IssuesGetRequest.java
+++ src/main/java/com/example/demo/dto/issue/IssuesGetRequest.java
@@ -15,13 +15,13 @@ public class IssuesGetRequest implements ToUser {
     private String username;
     private String password;
 
-    private String title = null;
-    private String reporter = null;
-    private String fixer = null;
-    private String assignee = null;
-    private IssuePriority priority = null;
-    private IssueStatus status = null;
-    private LocalDateTime reportedDate = null;
+    private String title;
+    private String reporter;
+    private String fixer;
+    private String assignee;
+    private IssuePriority priority;
+    private IssueStatus status;
+    private LocalDateTime reportedDate;
 
     public User toUser(){
         return new User(username, password);
