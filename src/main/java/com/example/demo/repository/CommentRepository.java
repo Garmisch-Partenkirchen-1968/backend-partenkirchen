f58a631bea1bbd40e1c9bfd6d7f40a6e9ea8ed2c
zzeo14 <wodh7223@cau.ac.kr>
2024-05-19 18:10:26 +0900

    update: test for comment post

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -3,5 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
