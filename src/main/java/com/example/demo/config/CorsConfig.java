3e52d6c5ac4d6a7694af9086c554857e2305f9e0
Youngho Cho <dominoyh5@gmail.com>
2024-05-19 21:33:27 +0900

    add: cors config

---
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import jakarta.annotation.Nonnull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@Nonnull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowedHeaders("*")
+                        .maxAge(3600);
+            }
+        };
+    }
+}
