a0b4c6518717d255ef513d4c0dd0bc21cf80cc4d
zzeo14 <wodh7223@cau.ac.kr>
2024-05-19 14:28:23 +0900

    update: rename function

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
-    Optional<User> getReferenceByUsername(String reporterUsername);
+    Optional<User> getReferenceByUsername(String username);
 }
