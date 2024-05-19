a46212e30684c50011fcebbda9f40ef745725926
zzeo14 <wodh7223@cau.ac.kr>
2024-05-19 14:51:41 +0900

    update Comment Tests

--- src/test/java/com/example/demo/comment/CommentDeleteTest.java
+++ src/test/java/com/example/demo/comment/CommentDeleteTest.java
@@ -1,7 +1,12 @@
 package com.example.demo.comment;
 
+import com.example.demo.entity.User;
+import com.example.demo.repository.IssueRepository;
+import com.example.demo.repository.ProjectRepository;
+import com.example.demo.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,4 +23,18 @@ public class CommentDeleteTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private IssueRepository issueRepository;
+
+    @BeforeEach
+    void init() throws Exception {
+        //
+    }
 }
--- src/test/java/com/example/demo/comment/CommentPostTest.java
+++ src/test/java/com/example/demo/comment/CommentPostTest.java
@@ -1,4 +1,66 @@
 package com.example.demo.comment;
 
+import com.example.demo.entity.User;
+import com.example.demo.repository.IssueRepository;
+import com.example.demo.repository.ProjectRepository;
+import com.example.demo.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@ActiveProfiles
+@AutoConfigureMockMvc
+@Transactional
 public class CommentPostTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private IssueRepository issueRepository;
+
+    @BeforeEach
+    void init() throws Exception {
+        // admin 생성
+        User admin = User.builder()
+                .username("admin")
+                .password("admin")
+                .build();
+        userService.signUpUser(admin);
+
+        // issue를 추가할 tester 생성
+        User tester = User.builder()
+                .username("tester")
+                .password("tester")
+                .build();
+        userService.signUpUser(tester);
+
+        // comment를 추가할 dev 생성
+        User dev1 = User.builder()
+                .username("dev1")
+                .password("dev1")
+                .build();
+        userService.signUpUser(dev1);
+
+        // comment를 추가할 another dev 생성
+        User dev2 = User.builder()
+                .username("dev2")
+                .password("dev2")
+                .build();
+        userService.signUpUser(dev2);
+    }
 }
