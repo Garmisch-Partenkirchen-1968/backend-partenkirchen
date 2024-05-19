3ec376403ebd59cf9280158a663683c678c48ef5
Youngho Cho <dominoyh5@gmail.com>
2024-05-19 17:12:02 +0900

    add: check requester in CommentController

--- src/main/java/com/example/demo/controller/CommentController.java
+++ src/main/java/com/example/demo/controller/CommentController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.comment.CommentDeleteRequest;
+import com.example.demo.dto.comment.CommentPatchRequest;
 import com.example.demo.dto.comment.CommentPostRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,18 +9,30 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CommentController {
+    private final UserFindController userFindController;
+
+    public CommentController(UserFindController userFindController) {
+        this.userFindController = userFindController;
+    }
+
     @PostMapping("/projects/{projectId}/issues/{issueId}/comments")
     public ResponseEntity postComment(@PathVariable("projectId") Long projectId, @PathVariable("issueId") Long issueId, @RequestBody CommentPostRequest commentPostRequest) {
+        userFindController.RequesterIsFound(commentPostRequest);
+
         return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @PatchMapping("/projects/{projectId}/issues/{issueId}/comments/{commentId}")
-    public ResponseEntity patchComment(@PathVariable("projectId") Long projectId, @PathVariable("issueId") Long issueId, @RequestBody CommentPostRequest commentPostRequest) {
+    public ResponseEntity patchComment(@PathVariable("projectId") Long projectId, @PathVariable("issueId") Long issueId, @RequestBody CommentPatchRequest commentPatchRequest) {
+        userFindController.RequesterIsFound(commentPatchRequest);
+
         return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @DeleteMapping("/projects/{projectId}/issues/{issueId}/comments/{commentId}")
     public ResponseEntity deleteComment(@PathVariable("projectId") Long projectId, @PathVariable("issueId") Long issueId, @RequestBody CommentDeleteRequest commentDeleteRequest) {
+        userFindController.RequesterIsFound(commentDeleteRequest);
+
         return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
