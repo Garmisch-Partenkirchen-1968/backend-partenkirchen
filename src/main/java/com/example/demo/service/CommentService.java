7bd9647ac11f36b2bcc15b5ad913e46775132348
Youngho Cho <dominoyh5@gmail.com>
2024-05-19 17:38:12 +0900

    add: getter in CommentService

--- src/main/java/com/example/demo/service/CommentService.java
+++ src/main/java/com/example/demo/service/CommentService.java
@@ -1,15 +1,43 @@
 package com.example.demo.service;
 
+import com.example.demo.controller.CommentController;
 import com.example.demo.dto.comment.CommentDeleteRequest;
 import com.example.demo.dto.comment.CommentPatchRequest;
 import com.example.demo.dto.comment.CommentPostRequest;
 import com.example.demo.dto.comment.CommentPostResponse;
 import com.example.demo.entity.Comment;
+import com.example.demo.entity.Issue;
+import com.example.demo.entity.Project;
+import com.example.demo.entity.User;
+import com.example.demo.repository.CommentRepository;
+import com.example.demo.repository.IssueRepository;
+import com.example.demo.repository.ProjectRepository;
+import com.example.demo.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 public class CommentService {
+    private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
+    private final IssueRepository issueRepository;
+    private final CommentRepository commentRepository;
+    private final CommentController commentController;
+
+    public CommentService(ProjectRepository projectRepository, UserRepository userRepository, IssueRepository issueRepository, CommentRepository commentRepository, CommentController commentController) {
+        this.projectRepository = projectRepository;
+        this.userRepository = userRepository;
+        this.issueRepository = issueRepository;
+        this.commentRepository = commentRepository;
+        this.commentController = commentController;
+    }
+
     public CommentPostResponse postComment(Long projectId, Long issueId, Long userId, CommentPostRequest commentPostRequest) {
+
+
         return null;
     }
 
@@ -17,5 +45,37 @@ public class CommentService {
     }
 
     public void deleteComment(Long projectId, Long issueId, Long commentId, Long userId, CommentDeleteRequest commentDeleteRequest) {
+    }
+
+    private Project getProject(Long projectId) {
+        Optional<Project> optionalProject = projectRepository.findById(projectId);
+        if (optionalProject.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "project not found");
+        }
+        return optionalProject.get();
+    }
+
+    private Issue getIssue(Long issueId) {
+        Optional<Issue> optionalIssue = issueRepository.findById(issueId);
+        if (optionalIssue.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "issue not found");
+        }
+        return optionalIssue.get();
+    }
+
+    private User getUser(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "user not found");
+        }
+        return optionalUser.get();
+    }
+
+    private Comment getComment(Long commentId) {
+        Optional<Comment> optionalComment = commentRepository.findById(commentId);
+        if (optionalComment.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "comment not found");
+        }
+        return optionalComment.get();
     }
 }
