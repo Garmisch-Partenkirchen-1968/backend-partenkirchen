0f032d267980ec4aa72ac1f058d9e0e706161237
Youngho Cho <dominoyh5@gmail.com>
2024-05-19 17:09:04 +0900

    add: implement ToUser

--- src/main/java/com/example/demo/dto/comment/CommentDeleteRequest.java
+++ src/main/java/com/example/demo/dto/comment/CommentDeleteRequest.java
@@ -1,11 +1,23 @@
 package com.example.demo.dto.comment;
 
+import com.example.demo.Interface.ToUser;
+import com.example.demo.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @Getter
 @AllArgsConstructor
-public class CommentDeleteRequest {
+public class CommentDeleteRequest implements ToUser {
     private String username;
     private String password;
+
+    @Override
+    public User toUser() {
+        if (username == null || password == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+        return new User(username, password);
+    }
 }
--- src/main/java/com/example/demo/dto/comment/CommentPatchRequest.java
+++ src/main/java/com/example/demo/dto/comment/CommentPatchRequest.java
@@ -1,13 +1,25 @@
 package com.example.demo.dto.comment;
 
+import com.example.demo.Interface.ToUser;
+import com.example.demo.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @Getter
 @AllArgsConstructor
-public class CommentPatchRequest {
+public class CommentPatchRequest implements ToUser {
     private String username;
     private String password;
     private String content;
     private Boolean isDescription;
+
+    @Override
+    public User toUser() {
+        if (username == null || password == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+        return new User(username, password);
+    }
 }
