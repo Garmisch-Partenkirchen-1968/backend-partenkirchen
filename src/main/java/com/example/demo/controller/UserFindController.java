a44c5f37f24bcdf1cd892fa3ea46904e6e967e69
Youngho Cho <dominoyh5@gmail.com>
2024-05-25 22:45:12 +0900

    update: apply hashing to UserFindController.RequesterIsFound

--- src/main/java/com/example/demo/controller/UserFindController.java
+++ src/main/java/com/example/demo/controller/UserFindController.java
@@ -4,6 +4,7 @@ import com.example.demo.Interface.ToUser;
 import com.example.demo.dto.user.UserSignInResponse;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.service.BCryptService;
 import com.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserFindController {
     private final UserRepository userRepository;
+    private final BCryptService bCryptService;
 
     public Long RequesterIsFound(ToUser toUser){
         Optional<User> req = userRepository.findByUsername(toUser.toUser().getUsername());
@@ -24,7 +26,8 @@ public class UserFindController {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found");
         }
         User requester = req.get();
-        if(!requester.getPassword().equals(toUser.toUser().getPassword())) {
+        String encodedPassword = bCryptService.encodeBcrypt(requester.getPassword(), 23);
+        if(!encodedPassword.equals(toUser.toUser().getPassword())) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Passwords do not match");
         }
         return requester.getId();
