080b7262a7886452652ed7b5860c056e56c18dd3
Youngho Cho <dominoyh5@gmail.com>
2024-05-25 23:11:52 +0900

    remove: unnecessary code in IssueService

--- src/main/java/com/example/demo/service/IssueService.java
+++ src/main/java/com/example/demo/service/IssueService.java
@@ -20,6 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -36,21 +37,9 @@ public class IssueService {
     }
 
     public IssuePostResponse postIssue(Long projectId, IssuePostRequest issuePostRequest) {
-        User us = issuePostRequest.toUser();
+        User user = getUserByUsername(issuePostRequest.toUser().getUsername());
         Optional<Project> proj = projectRepository.findById(projectId);
         System.out.println("PostIssue");
-
-        // User가 없는 경우
-        if(userRepository.findByUsername(us.getUsername()).isEmpty()) {
-            System.out.println("User not found");
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found");
-        }
-        User user = userRepository.findByUsername(us.getUsername()).get();
-
-        if(!user.getPassword().equals(us.getPassword())){
-            System.out.println("Wrong password");
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Wrong password");
-        }
 
         // project가 없는 경우
         if (proj.isEmpty()) {
@@ -72,7 +61,7 @@ public class IssueService {
         }
 
         // title 입력이 없는 경우
-        if(issuePostRequest.getTitle().equals("")){
+        if(issuePostRequest.getTitle().isEmpty()){
             System.out.println("Title is required");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Title is required");
         }
@@ -87,8 +76,7 @@ public class IssueService {
         project.getIssues().add(issue);
         projectRepository.save(project);
 
-        IssuePostResponse issuePostResponse = new IssuePostResponse(issue.getId(), issue.getTitle(), issue.getPriority());
-        return issuePostResponse;
+        return new IssuePostResponse(issue.getId(), issue.getTitle(), issue.getPriority());
     }
 
     public List<Issue> getIssues(Long projectId, IssuesGetRequest issuesGetRequest) {
@@ -99,19 +87,7 @@ public class IssueService {
         }
 
         Project project = proj.get();
-        User us = issuesGetRequest.toUser();
-        // user가 존재하지 않을 때
-        if(userRepository.findByUsername(us.getUsername()).isEmpty()) {
-            System.out.println("User not found");
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found");
-        }
-        User user = userRepository.findByUsername(us.getUsername()).get();
-
-        // 비밀번호가 틀렸을 때
-        if(!user.getPassword().equals(us.getPassword())){
-            System.out.println("Wrong password");
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Wrong password");
-        }
+        User user = getUserByUsername(issuesGetRequest.toUser().getUsername());
 
         // user가 project의 member가 아닐 때
         if (project.getMembers().get(user) == null) {
@@ -158,7 +134,7 @@ public class IssueService {
         // Asignee가 요청으로 왔을 때
         if (issuesGetRequest.getAssignee() != null) {
             Optional<User> assign = userRepository.findByUsername(issuesGetRequest.getAssignee());
-            if (issuesGetRequest.getAssignee().equals("")) { // assignee 배정 안 된 issue 찾기
+            if (issuesGetRequest.getAssignee().isEmpty()) { // assignee 배정 안 된 issue 찾기
                 issues.removeIf(issue -> issue.getAssignee() != null);
             } else {
                 // assignee가 존재하는지
@@ -188,8 +164,7 @@ public class IssueService {
 
         Project project = PI.a;
         Issue issue = PI.b;
-        User us = issueGetRequest.toUser();
-        User user = FindUser(us);
+        User user = getUserByUsername(issueGetRequest.getUsername());
 
         if (project.getMembers().get(user) == null) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User is not tester");
@@ -204,24 +179,12 @@ public class IssueService {
         Issue issue = PI.b;
         Optional<User> assign = userRepository.findByUsername(issuePatchRequest.getAssignee());
 
-        User us = issuePatchRequest.toUser();
-        // user가 없는 사람일 때
-        if(userRepository.findByUsername(us.getUsername()).isEmpty()) {
-            System.out.println("User not found");
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found");
-        }
-        User user = userRepository.findByUsername(us.getUsername()).get();
+        User user = getUserByUsername(issuePatchRequest.toUser().getUsername());
 
         // user가 project 소속 인원이 아닐 때
         if(project.getMembers().get(user) == null) {
             System.out.println("User is not member of project");
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User is not member of project");
-        }
-
-        // 비밀번호가 틀렸을 때
-        if(!user.getPassword().equals(us.getPassword())){
-            System.out.println("Wrong password");
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Wrong password");
         }
 
         Integer userPermission = project.getMembers().get(user);
@@ -298,17 +261,11 @@ public class IssueService {
 
         Project project = PI.a;
         Issue issue = PI.b;
-        User us = issueDeleteRequest.toUser();
-        User user = FindUser(us);
 
-        // User가 Project에 없으면
-        if(project.getMembers().get(user) == null){
-            System.out.println("User not found");
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User not found");
-        }
+        User user = getUserByUsername(issueDeleteRequest.toUser().getUsername());
 
         // User가 Reporter가 아니면
-        if (issue.getReporter() != user) {
+        if (!Objects.equals(issue.getReporter().getId(), user.getId())) {
             System.out.println("User is not reporter");
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User is not reporter");
         }
@@ -334,21 +291,11 @@ public class IssueService {
         return new Pair<>(project.get(), issue.get());
     }
 
-    private User FindUser(User ToUser){
-        Optional<User> Optional_user = userRepository.findByUsername(ToUser.getUsername());
-        // User가 없는 사람일 때
-        if(Optional_user.isEmpty()){
-            System.out.println("User not found");
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found");
+    private User getUserByUsername(String username) {
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+        if (optionalUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "user not found");
         }
-
-        // User의 Password가 틀렸을 때
-        User user = Optional_user.get();
-        if(!user.getPassword().equals(ToUser.getPassword())){
-            System.out.println("Password not match");
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Password not match");
-        }
-
-        return user;
+        return optionalUser.get();
     }
 }
